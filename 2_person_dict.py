04b609fa8b27adb09ac7972f334fccd925e864ae
Luke <luke_albert1@baylor.edu>
2023-01-30 14:12:40 -0600

    class changes

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,22 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+# name of second child
+print(person["children"][1])  # to just get betty
+print(type(person["children"]))
+print(person["children"][1])
+
+# the name of the cat
+print(type(person["pets"]))
+print(person)
+
+# use a fr loop
+for i in person["children"]:
+    print(i)
+
+# type of pet: dog name of pet: Fido'
+for i, j in person["pets"].items():
+    print(f"Type of pet:{i} name of [et: {j}")
